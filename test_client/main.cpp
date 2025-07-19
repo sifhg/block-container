e6b42b15353b08dbf8b997f7c02afeb201adaf61
Sif Høg-Getgood <dev@hg-mail.eu>
2025-07-19 16:52:37 +0200

    Verify Block's memory size

--- test_client/main.cpp
+++ test_client/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <vector>
 #include "Block.hpp"
+
+struct positionComponent
+{
+    int x;
+    int y;
+    int z;
+};
+
 
 int main()
 {
     auto intBlock = Block<int>::CreateBlock(3);
     std::cout << "Allocated size of Block<int> is: " << intBlock.GetAllocatedSize() << std::endl;
+    std::cout << "Size of intBlock in memory is " << sizeof(intBlock) << ".\n";
     int* intIndex0 = intBlock.Push(500);
     std::cout << "Pushed int to block" << *intIndex0 << ".\n";
     std::cout << "Block size is: " << intBlock.GetSize() << ".\n";
@@ -35,6 +45,7 @@ int main()
     std::cout << "Block allocates " << intBlock.GetAllocatedSize() << ".\n\n"; 
 
     std::cout << "intIndex5's address is " << intIndex5 << ".\n";
+    std::cout << "Size of intBlock in memory is " << sizeof(intBlock) << ".\n";
     std::cout << "Delete data at intIndex5.\n";
     intBlock.Delete(intIndex5);
     std::cout << "intIndex5's address is now " << intIndex5 << ".\n";
@@ -42,6 +53,14 @@ int main()
     int* intIndex5b = intBlock.Push(1000);
     std::cout << "intIndex5b's address is " << intIndex5b << ".\n";
     std::cout << "intIndex5's value is " << *intIndex5b << ".\n";
+    std::cout << "Size of intBlock in memory is " << sizeof(intBlock) << ".\n";
+
+    std::cout << "sizeof(Block<int>): " << sizeof(Block<int>) << ".\n";
+    std::cout << "sizeof(Block<float>): " << sizeof(Block<float>) << ".\n";
+    std::cout << "sizeof(Block<double>): " << sizeof(Block<double>) << ".\n";
+    std::cout << "sizeof(Block<char>): " << sizeof(Block<char>) << ".\n";
+    std::cout << "sizeof(Block<std::string>): " << sizeof(Block<std::string>) << ".\n";
+    std::cout << "sizeof(Block<positionComponent>): " << sizeof(Block<positionComponent>) << ".\n";
 
     return 0;
 }
