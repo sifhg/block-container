502f73aa96f680e6282f9120b6e36154ec57fd8d
Sif Høg-Getgood <dev@hg-mail.eu>
2025-07-27 16:44:58 +0200

    Add tests for bad allocation

--- test_client/BlockTest.cpp
+++ test_client/BlockTest.cpp
@@ -44,7 +44,7 @@ std::shared_ptr<Test> BlockTest::GetTest()
     }
   );
   testPtr->AddFeatureTest(
-    "Attempt to create an Block that allocates 0 elements",
+    "Attempt to create a Block that allocates 0 elements",
     {"CreateBlock"},
     []{
       try {
@@ -56,6 +56,57 @@ std::shared_ptr<Test> BlockTest::GetTest()
       throw std::runtime_error("Block::CreateBlock with argument 0 threw no error.");
     }
   );
-  // TODO: Write a test for negative allocation
+  testPtr->AddFeatureTest(
+    "Attempt to create an int Block that allocates -1 elements",
+    {"CreateBlock"},
+    []{
+      try {
+        auto intBlock = Block<int>::CreateBlock(-1); 
+      } catch (int errorCode) {
+        return;
+      }
+      throw std::runtime_error("Block::CreateBlock with argument -1 threw no error.");
+    }
+  );
+  testPtr->AddFeatureTest(
+    "Attempt to create a double Block that allocates -256 elements",
+    {"CreateBlock"},
+    []{
+      try {
+        auto intBlock = Block<double>::CreateBlock(-256); 
+      }
+      catch (int errorCode) {
+        return;
+      }
+      throw std::runtime_error("Block::CreateBlock with argument -256 threw no error.");
+    }
+  );
+  testPtr->AddFeatureTest(
+    "Attempt to create a char Block that allocates INT_MIN elements",
+    {"CreateBlock"},
+    []{
+      try {
+        auto intBlock = Block<char>::CreateBlock(INT_MIN); 
+      }
+      catch (int errorCode) {
+        return;
+      }
+      throw std::runtime_error("Block::CreateBlock with argument INT_MIN threw no error.");
+    }
+  );
+  testPtr->AddFeatureTest(
+    "Attempt to create a double Block that allocates INT_MAX elements",
+    {"CreateBlock"},
+    []{
+      try {
+        auto intBlock = Block<double>::CreateBlock(INT_MIN); 
+      }
+      catch (int errorCode) {
+        throw std::runtime_error("Could not Block::CreateBlock with INT_MAX elements.");
+      }
+    }
+  );
+  
+  // TODO: Rewrite the Block class so it uses size_t but CreateBlock still takes signed integers as input, as a fail safe if the developer tries to input negative values
   return testPtr;
 }
